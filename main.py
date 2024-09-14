4d41499302e28ef790196385c7448ad2b80146bf
Bhaswanth Kumar Bonthala <125963613+Bhaswanth67@users.noreply.github.com>
2024-09-15 00:48:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 # Google Drive file ID and file path
 FILE_ID = "1Z8oMCp1XR3sFq2iK034RCnTj_2RbqRKN"
-MODEL_PATH = "trained_plant_disease_model.keras"
+MODEL_PATH = "trained_plant_disease_model.h5"
 MODEL_URL = f"https://drive.google.com/uc?export=download&id={FILE_ID}"
 
 # Function to download the model if not present
