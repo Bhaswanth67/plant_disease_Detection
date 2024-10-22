11ecb2045ad874cc65e3011fb5e0139689be0b9e
Bhaswanth Kumar Bonthala <125963613+Bhaswanth67@users.noreply.github.com>
2024-10-22 15:45:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,6 @@ import tensorflow as tf
 import numpy as np
 import streamlit as st
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 
 st.set_page_config(
