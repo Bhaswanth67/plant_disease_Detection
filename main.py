df420be21224127f4e5854055ba5ad02a4d99da0
Bhaswanth Kumar Bonthala <125963613+Bhaswanth67@users.noreply.github.com>
2025-01-21 12:27:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -113,8 +113,7 @@ elif app_mode == "About":
 
     #### Project Team
     This project is developed by:
-    - **Sujit Varma K**
-    - **Suhaas NV**
+
     - **Bhaswanth Kumar B**
 
     Our team is dedicated to creating an efficient and accurate plant disease recognition system to help in protecting crops and ensuring a healthier harvest.
